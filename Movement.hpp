d815c8b2ca7ffb0bd85d90fd8fdd9862e40e975f
CW-X <101200450+CW-X@users.noreply.github.com>
2022-03-16 22:40:03 +0100

    Update Movement.hpp

--- Movement.hpp
+++ Movement.hpp
@@ -1,24 +1,71 @@
-#pragma once
+//by Changwen
+#define R_EN  10
+#define R1     9
+#define R2     8
 
-#include <Arduino.h>
-#include "constants.cpp"
+#define L_EN   5
+#define L1     7
+#define L2     6
+ 
+#define slow 64
+#define normal 128
+#define fast 255
+ 
+int Speed; 
+ 
+void Forward(void){
+  analogWrite(R_EN, Speed);
+  analogWrite(L_EN, Speed);
+  digitalWrite(R1, HIGH);
+  digitalWrite(R2 , LOW);
+  digitalWrite(L1, HIGH);
+  digitalWrite(L2 , LOW);
+}
+ 
+void Backward(void){
+  analogWrite(R_EN, Speed);
+  analogWrite(L_EN, Speed);
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, HIGH);
+  digitalWrite(L1, LOW);
+  digitalWrite(L2, HIGH);
+}
+ 
 
-class Movement
-{
-public:
-  Movement();
-  void Update();
-  void Turn(int degree);
-  void StartMoving();
-  void StopMoving();
-private:
-  int m_AmountToTurn;
-};
 
+void Turnright(void){
+  analogWrite(R_EN, Speed);
+  analogWrite(L_EN, Speed);
+  digitalWrite(R1, HIGH);
+  digitalWrite(R2, LOW);
+  digitalWrite(L1, LOW);
+  digitalWrite(L2, HIGH);
+}
+ 
 
-Movement::Movement() {}
-void Movement::Update() {}
-void Movement::Turn(int degree) {}
-void Movement::StartMoving() {}
-void Movement::StopMoving() {}
+void Turnleft(void){
+  analogWrite(R_EN, Speed);
+  analogWrite(L_EN, Speed);
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, HIGH);
+  digitalWrite(L1, HIGH);
+  digitalWrite(L2, LOW);
 
+}
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(R_EN, OUTPUT);
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(L_EN, OUTPUT);
+  pinMode(R1, OUTPUT);
+  pinMode(R2, OUTPUT);
+}
+ 
+void loop() {
+
+  Speed=normal; // Normal Speed
+  Turnleft();
+  delay(3000);
+}
